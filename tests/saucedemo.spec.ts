175009a34e87103152b2e73e64da1ab431f0704d
Kenza Agatha <kenzaagatha27@gmail.com>
2026-05-28 12:41:27 +0700

    updated test scenarios

--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -2,67 +2,51 @@ import { test, expect } from '@playwright/test';
 
 test('Skenario 1: User harus bisa login dengan akun valid', async ({ page }) => {
 
-    // 1. NAVIGATE: Suruh robot buka website tujuan
+    // 1. NAVIGATE
     await page.goto('https://www.saucedemo.com/');
 
     // 2. INTERACT: Isi form login
-    // Kita cari kolom input yang punya id="user-name", lalu ketik 'standard_user'
     await page.locator('[data-test="username"]').fill('standard_user');
-
-    // Kita cari kolom input yang punya id="password", lalu ketik 'secret_sauce'
     await page.locator('[data-test="password"]').fill('secret_sauce');
-
-    // 3. INTERACT: Klik tombol login
-    // Kita cari elemen tombol yang punya id="login-button", lalu klik
     await page.locator('[data-test="login-button"]').click();
 
-    // 4. ASSERTION: Mastiin kalau kita beneran sukses masuk
-    // Kita cek apakah setelah diklik, URL browsernya berubah jadi halaman inventory (katalog produk)
+    // 3. ASSERTION: Memastikan user sukses masuk ke dashboard
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
-
-    // Kita cek juga apakah ada tulisan "Products" di dashboard sebagai bukti halaman sudah ke-load
     const headerTitle = page.locator('.title');
     await expect(headerTitle).toHaveText('Products');
 });
 
 test('Skenario 2: User harus mendapatkan eror jika login dengan password salah', async ({ page }) => {
 
-    // 1. NAVIGATE: Buka web
+    // 1. NAVIGATE
     await page.goto('https://www.saucedemo.com/');
 
     // 2. INTERACT: Isi form login
     await page.locator('[data-test="username"]').fill('standard_user');
-
-    // SENGJA SALAH: Masukkan password 'salah_sauce' (harusnya 'secret_sauce')
     await page.locator('[data-test="password"]').fill('salah_sauce');
-
-    // 3. INTERACT: Klik login
     await page.locator('[data-test="login-button"]').click();
 
-    // 4. ASSERTION: Validasi pesan eror
-    // Kita cari elemen penampung eror dan pastikan dia kelihatan di layar
+    // 3. ASSERTION: Validasi pesan eror
     const errorContainer = page.locator('[data-test="error"]');
     await expect(errorContainer).toBeVisible();
 
-    // Mastiin isi teks erornya bener, bukan asal eror
+    // Memastikan pesan error sesuai
     await expect(errorContainer).toHaveText(/Epic sadface: Username and password do not match/);
-
-    // Mastiin kita tetep diam di halaman login, gak pindah URL
     await expect(page).toHaveURL('https://www.saucedemo.com/');
 });
 
 test('Skenario 3: User harus bisa memasukkan item ke keranjang', async ({ page }) => {
+    // 1. NAVIGATE & LOGIN
     await page.goto('https://www.saucedemo.com/');
     await page.locator('[data-test="username"]').fill('standard_user');
     await page.locator('[data-test="password"]').fill('secret_sauce');
     await page.locator('[data-test="login-button"]').click();
-
+    // 2. INTERACT: Tambah item ke keranjang
     await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-
+    // 3. ASSERTION: Validasi item berhasil masuk ke keranjang
     const shoppingCartBadge = page.locator('[data-test="shopping-cart-badge"]');
-
+    // Memastikan ikon keranjang belanja muncul tanda ada item di dalamnya
     await expect(shoppingCartBadge).toBeVisible();
-
     await expect(shoppingCartBadge).toHaveText('1');
 });
 
@@ -95,11 +79,10 @@ test('Skenario 4: User harus bisa menyelesaikan proses checkout belanja sampai s
     // 7. INTERACT: Klik tombol Finish buat bayar/menyelesaikan orderan
     await page.locator('[data-test="finish"]').click();
 
-    // 8. ASSERTION FINAL: Mastiin orderan sukses dikirim
-    // Kita cek apakah URL-nya sudah berubah ke halaman checkout-complete
+    // 8. ASSERTION FINAL: Memastikan orderan sukses dikirim
     await expect(page).toHaveURL('https://www.saucedemo.com/checkout-complete.html');
 
-    // Kita pastikan muncul teks "Thank you for your order!" sebagai bukti valid
+    // Akan muncul teks "Thank you for your order!" sebagai bukti valid
     const successHeader = page.locator('[data-test="complete-header"]');
     await expect(successHeader).toHaveText('Thank you for your order!');
 });
